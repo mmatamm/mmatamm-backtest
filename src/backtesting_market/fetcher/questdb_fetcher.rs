d985555afa45118e3582c6d8c1036cd8787114a0
Amit Goren <animalfarm@mailfence.com>
2025-05-03 00:36:00 +0300

    style: remove old commented code

--- src/backtesting_market/fetcher/questdb_fetcher.rs
+++ src/backtesting_market/fetcher/questdb_fetcher.rs
@@ -34,58 +34,6 @@ impl QuestDbFetcher {
 impl Fetcher for QuestDbFetcher {
     type Error = Error;
 
-    // async fn query_price(
-    //     &mut self,
-    //     time: &DateTime<Utc>,
-    //     symbol: &str,
-    // ) -> Result<Option<f32>, Error> {
-    //     let row_opt = self
-    //         .db_client
-    //         .query_opt(
-    //             &self.price_query_statement,
-    //             &[&(time.timestamp_micros() as f64), &symbol],
-    //         )
-    //         .await?;
-
-    //     if let Some(row) = row_opt {
-    //         Ok(Some(row.get(4)))
-    //     } else {
-    //         Ok(None)
-    //     }
-    // }
-
-    // async fn query_system_event(
-    //     &mut self,
-    //     time: &DateTime<Utc>,
-    // ) -> Result<Option<(SystemEvent, NaiveDateTime)>, Error> {
-    //     let row_opt = self
-    //         .db_client
-    //         .query_opt(
-    //             &self.system_event_query_statement,
-    //             &[&(time.timestamp_micros() as f64)],
-    //         )
-    //         .await?;
-
-    //     if let Some(row) = row_opt {
-    //         let event_type_str: String = row.get(0);
-
-    //         let event_type = match event_type_str.as_str() {
-    //             "system_hours_start" => Ok(SystemEvent::PreMarketStart),
-    //             "regular_hours_start" => Ok(SystemEvent::RegularMarketStart),
-    //             "regular_hours_end" => Ok(SystemEvent::RegularMarketEnd),
-    //             "system_hours_end" => Ok(SystemEvent::PostMarketEnd),
-    //             symbol => Err(Error::UnexpectedDatabaseSymbol {
-    //                 symbol: symbol.to_string(),
-    //                 expected_kind: "system event".to_string(),
-    //             }),
-    //         }?;
-
-    //         Ok(Some((event_type, row.get(1))))
-    //     } else {
-    //         Ok(None)
-    //     }
-    // }
-
     // TODO I want to be able to query only a range
     fn fetch_system_events(&mut self) -> Result<Vec<(i64, SystemEvent)>, Self::Error> {
         let rows = self
@@ -143,19 +91,6 @@ impl Fetcher for QuestDbFetcher {
                 Ok((timestamp, ohlc))
             })
             .collect()
-        //     let row_opt = self
-        //         .db_client
-        //         .query_opt(
-        //             &self.price_query_statement,
-        //             &[&(time.timestamp_micros() as f64), &symbol],
-        //         )
-        //         .await?;
-
-        //     if let Some(row) = row_opt {
-        //         Ok(Some(row.get(4)))
-        //     } else {
-        //         Ok(None)
-        //     }
     }
 }
 
